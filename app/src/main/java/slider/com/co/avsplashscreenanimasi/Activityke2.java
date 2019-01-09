6a10a2611e1afd74055a70f5498b11cefb984017
raga <ragamuthahari@gmail.com>
2019-01-09 11:08:16 +0700

    Initial commit

---
+++ app/src/main/java/slider/com/co/avsplashscreenanimasi/Activityke2.java
@@ -0,0 +1,13 @@
+package slider.com.co.avsplashscreenanimasi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activityke2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityke2);
+    }
+}
